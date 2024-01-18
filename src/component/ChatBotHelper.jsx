d4ddb7370cbdcf1712152dd6ca08329fb197a2c1
sangeeth-swivel <sangeeth@swivelgroup.com.au>
2024-01-18 12:09:15 +0530

    update component

--- src/component/ChatBotHelper.jsx
+++ src/component/ChatBotHelper.jsx
@@ -2,76 +2,64 @@ import React from "react";
 import ChatBot from "react-simple-chatbot";
 import { ThemeProvider } from "styled-components";
 
-const BotRedirect = ({ url, message }) => {
+// Instruction given to chatbot and there trigger component
+const steps = [
+  {
+    id: '0',
+    message: 'Hello!',
+    trigger: '1', // Call the next step (id 1)
+  }, {
+    id: '1',
+    message: 'Please write your username',
+    trigger: '2', // Call step 2 for user input
+  }, {
+    id: '2',
+    user: true, // Enable user input
+    trigger: '3', // Call step 3 after input
+  }, {
+    id: '3',
+    message: "Hi {previousValue}, how can I help you?", // Access previous input
+    trigger: '4', // Call step 4 for options
+  }, {
+    id: '4',
+    options: [
+      { value: 1, label: 'View Courses' },
+      { value: 2, label: 'Read Articles' },
+    ],
+    end: true, // Mark as the final step
+  }
+];
+
+// Creating our own theme
+const theme = {
+  background: '#C9FF8F',
+  headerBgColor: '#197B22',
+  headerFontSize: '20px',
+  botBubbleColor: '#0F3789',
+  headerFontColor: 'white',
+  botFontColor: 'white',
+  userBubbleColor: '#FF5733',
+  userFontColor: 'white',
+};
+
+// Chatbot configuration
+const config = {
+  botAvatar: "chatbot-icon.png",
+  floating: true,
+};
+
+const ChatBotHelper = () => {
   return (
-    <div>
-      <a href={url} target="_blank">
-        {message}
-      </a>
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <ChatBot
+          headerTitle="Simple-chatbot"
+          steps={steps}
+          {...config}
+        />
+      </ThemeProvider>
     </div>
   );
 };
 
-//CSS AND STYLING
-const CHATBOT_THEME = {
-  background: "#FFFEFC",
-  fontFamily: "Roboto",
-  headerBgColor: "#FFBFB5",
-  headerFontColor: "#fff",
-  headerFontSize: "15px",
-  botBubbleColor: "#C8D7C2",
-  botFontColor: "#fff",
-  userBubbleColor: "#FFBFB5",
-  userFontColor: "#fff"
-};
-
-
-const ChatBotHelper = () => {
-  const steps = [
-    {
-      id: "1",
-      message: "Hello!",
-      trigger: "2"
-    },
-    {
-      id: "2",
-      message: "How can I help you?",
-      trigger: "3"
-    },
-    {
-      id: "3",
-      options: [
-        { value: 1, label: "Show ChatBot example", trigger: "4" },
-        { value: 2, label: "Show ChatBot API", trigger: "5" }
-      ]
-    },
-    {
-      id: "4",
-      component: (
-        <BotRedirect
-          message="See all examples in this page"
-          url="<https://lucasbassetti.com.br/react-simple-chatbot/#/docs/previous-value>"
-        />
-      ),
-      trigger: "2"
-    },
-    {
-      id: "5",
-      component: (
-        <BotRedirect
-          message="See chatbot API here"
-          url="<https://lucasbassetti.com.br/react-simple-chatbot/#/docs/chatbot>"
-        />
-      ),
-      trigger: "2"
-    }
-  ];
-  return (
-    <>
-      <ThemeProvider theme={CHATBOT_THEME}>
-        <ChatBot steps={steps} floating={true} />
-      </ThemeProvider>
-    </>
-  );
-};
 export default ChatBotHelper;
